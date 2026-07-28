ce6a799fbe1eda54a1a2aa6588b20a15003791a2
Renn <62453428+Renn9527@users.noreply.github.com>
2026-07-28 13:40:25 +0800

    chore: remove local machine paths

--- build_check.bat
+++ build_check.bat
@@ -1,5 +1,5 @@
 @echo off
 call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" amd64
-cd /d "F:\AI\vibecoding\format-checker\lintdrop\src-tauri"
+cd /d "%~dp0src-tauri"
 cargo check 2>&1
 echo EXIT_CODE=%ERRORLEVEL%
